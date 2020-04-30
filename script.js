a3ee3f57b36aa18c730b5a43f8be433dbfe5cd40
Vazily <vazily2015@gmail.com>
2020-04-30 19:09:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+
+'use strict';
+
+a = 5;
+console.log(a);
+
+/* var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14; */
+
+
+/* console.log('string'*90);
+
+let something;
+console.log(something);
+
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(person["age"]);
+ */
+
+let arr = ['plum.png','orange.jpeg','apple.bmp'];
+console.log(arr['0']);
+
+/* alert('Hello world'); */
+
+/* let answer = confirm('Are you here?');
+console.log(answer); */
+
+
+/* let answer = +prompt('Есть ли вам 18?', 'да');
+console.log(typeof(answer));
+
+console.log('arr' + ' - object');
+console.log(5 + +' - object'); */
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log('2' === '2');
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
